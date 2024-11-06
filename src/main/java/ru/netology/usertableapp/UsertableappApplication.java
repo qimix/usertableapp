c59554043620b43d1b850f6aabbf05c5cbe8ca14
Vitaliy Murzin <murzinvit@yandex.ru>
2024-11-07 00:21:27 +0500

    init commit

---
+++ src/main/java/ru/netology/usertableapp/UsertableappApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.usertableapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertableappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsertableappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/usertableapp/UsertableappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.usertableapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsertableappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
