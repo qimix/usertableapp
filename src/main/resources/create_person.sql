50c7115e2c45efa8b3c30633fe3b35a59cb6230a
Vitaliy Murzin <murzinvit@yandex.ru>
2024-11-12 04:55:15 +0500

    ref 5

--- src/main/resources/create_person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE PERSONS(
-    id Long,
-    name CHARACTER VARYING(100),
-    surname CHARACTER VARYING(100),
-    age INTEGER, phone_number BIGINT,
-    city_of_living CHARACTER VARYING(100),
-    PRIMARY KEY (name, surname, age)
-    );
